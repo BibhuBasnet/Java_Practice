603c2792697db03ae94d5d19b8869949fdc43636
Bibhu Basnet <bibhurbasnet50@gmail.com>
2022-06-17 01:54:05 -0400

    practice

---
+++ src/Assignment1.java
@@ -0,0 +1,20 @@
+public class Assignment1 {
+
+   public static void main(String[] args ){
+//      //  System.out.println("hello world");
+//       // int num = 2;
+//               // System.out.println(num);
+//       // Car mycar = new Car() ;
+//          //  System.out.print(mycar.name);
+//        int num1 = 10;
+//        int num2 = 20;
+//        int num3 = 30;
+//
+//        System.out.println(num1+num2+num3);
+//
+//    }
+
+       for (int x = 5 ; x < 10 ; x++){
+           System.out.println(" abc");
+       }
+}}
